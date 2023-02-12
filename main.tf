d2e2ec36ca3882f3d55f51dc8821f8119c924af2
Petya Gavrilova <petya.gavrilova@hashicorp.com>
2023-02-12 13:21:34 +0200

    committing changes

---
+++ main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "myec2" {
+ami = "ami-0aa7d40eeae50c9a9"
+instance_type = "t2.micro"
+count = 3
+
+tags = {
+  Name = "VM-${count.index}"
+}
+
+}
+variable "totalNum" {
+  default = "3"
+  
+}
+resource "null_resource" "localexec" {
+provisioner "local-exec" {
+    
+    command = <<EOT
+    echo 'Total number of VMs created is: ${var.totalNum}'
+    EOT
+  }
+
+}
+
+
+
+
+/*
+variable user {
+  type = list(string)
+  default = ["Petya", "Petko", "Gencho"]    
+  }
+
+variable "some_id" {
+ 
+}
+resource "null_resource" "screen_output" {
+
+  triggers = {
+    
+  }
+
+  provisioner "local-exec" {
+    
+    command = <<EOT
+    echo 'Hello ${var.user["${count.index}"]}'
+    EOT
+  }
+}
+*/
